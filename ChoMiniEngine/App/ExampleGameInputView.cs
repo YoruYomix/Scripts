750bf89b39c7b7a1a9c4910aeaf743ab144b9f53
Yoru <powersfwhite@gmail.com>
2025-12-13 15:24:14 +0900

    플레이랑 스킵을 어드밴스로 통합

---
+++ ChoMiniEngine/App/ExampleGameInputView.cs
@@ -0,0 +1,27 @@
+﻿using MessagePipe;
+using UnityEngine;
+using UnityEngine.UI;
+using Yoru.ChoMiniEngine;
+
+public class ExampleGameInputView : MonoBehaviour
+{
+    public Button _advanceButton;
+
+
+    private void Start()
+    {
+
+        _advanceButton = GetComponent<Button>();
+        _advanceButton.onClick.AddListener(OnSkipClicked);
+
+
+
+    }
+
+    private void OnSkipClicked()
+    {
+        ChoMiniCommand.Advance();
+        Debug.LogWarning("진행 버튼 클릭 됨");
+
+    }
+}
